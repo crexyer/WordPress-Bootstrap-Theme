503da186531b2c99a1f1acc2f7ca8e615730e372
crexyer@gmail.com <crexyer@gmail.com>
2013-12-26 22:27:56 +0800

    Fix comment bug

--- comments.php
+++ comments.php
@@ -20,4 +20,18 @@
 	</ol>
 	<?php endif; ?>
 	<?php bootstrap_paginate_comments_links(); ?>
+	<?php
+	comment_form ( array (
+			'comment_notes_before' => '',
+			'comment_notes_after' => '',
+			'fields' => array (
+					'author' => '<div class="form-group"><div class="col-sm-5"><input class="form-control" id="author" name="author" type="text" value="' . esc_attr ( $commenter ['comment_author'] ) . '" size="30" placeholder="' . __ ( 'Name', 'Bootstrap' ) . '"></div></div>',
+					
+					'email' => '<div class="form-group"><div class="col-sm-5"><input class="form-control" id="email" name="email" type="text" value="' . esc_attr ( $commenter ['comment_author_email'] ) . '" size="30" placeholder="' . __ ( 'Email', 'Bootstrap' ) . '"></div></div>',
+					
+					'url' => '<div class="form-group"><div class="col-sm-5"><input class="form-control" id="url" name="url" type="text" value="' . esc_attr ( $commenter ['comment_author_url'] ) . '" size="30" placeholder="' . __ ( 'Website (optional)', 'Bootstrap' ) . '"></div></div>' 
+			),
+			'comment_field' => '<div class="form-group"><div class="col-sm-12"><textarea class="form-control" id="comment" name="comment" cols="45" rows="4" aria-required="true" placeholder="' . __ ( 'Write your comment here.', 'Bootstrap' ) . '"></textarea></div></div>' 
+	) );
+	?>
 </div>
