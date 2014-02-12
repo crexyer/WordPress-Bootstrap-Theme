0e59582ffece4b104c3e2ce69b1e62a488f010ff
crexyer@gmail.com <crexyer@gmail.com>
2014-02-13 01:24:50 +0800

    Fix small bugs

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 		<form method="get" class="input-group search-box" action="<?php echo site_url(); ?>">
 			<input type="search" class="form-control" value="<?php echo get_search_query(); ?>" name="s" placeholder="<?php _e('Search', 'Bootstrap'); ?>">
 			<span class="input-group-btn">
-				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+				<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
 			</span>
 		</form>
 		<?php dynamic_sidebar( 1 ); ?>
