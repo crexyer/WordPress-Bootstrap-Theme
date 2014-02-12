c7bdce3279f3b54d427807a214176c44e5d3e4fa
crexyer@gmail.com <crexyer@gmail.com>
2014-02-13 01:03:13 +0800

    Modify stylesheet

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/bootstrap.css' ; ?>" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/bootstrap-theme.css' ; ?>" type="text/css" />
 	<link rel="stylesheet" href="<?php bloginfo ( 'stylesheet_url' );?>" />
 	<title><?php wp_title ( '|', true, 'right' ); ?></title>
 	<?php echo stripslashes ( get_option ( 'analytics_code', '' ) ); ?>
